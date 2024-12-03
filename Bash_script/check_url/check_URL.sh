784bad9ffe513724f04cc91c408470b210a97157
DylanBarboza <barbozadylan44@gmail.com>
2024-12-03 04:32:20 +0000

    Subo check-urls

--- Bash_script/check_url/check_URL.sh
+++ Bash_script/check_url/check_URL.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
-clear
 
-###############################
-#
-# Parametros:
-#  - Lista Dominios y URL
-#
-#  Tareas:
-#  - Se debera generar la estructura de directorio pedida con 1 solo comando con las tecnicas enseñadas en clases
-#  - Generar los archivos de logs requeridos.
-#
-###############################
-LISTA=$1
 
-LOG_FILE="/var/log/status_url.log"
+	#ruta del archivo
+LISTA="./Lista_URL.txt"
+
+# Fecha y hora actual
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
 ANT_IFS=$IFS
 IFS=$'\n'
 
-#---- Dentro del bucle ----#
-  # Obtener el código de estado HTTP
-  STATUS_CODE=$(curl -LI -o /dev/null -w '%{http_code}\n' -s "$URL")
+			#bucle para procesar cada linea de archivo ##
+for line in $(grep -v '^#' "$LISTA" | grep -v '^[[:space:]]*$' | grep -v '^#################################$' | grep -v '^$'); do
+    
+    # filtro por dominio y url
+    DOMAIN=$(echo "$line" | awk '{print $1}')
+    URL=$(echo "$line" | awk '{print $2}')
 
-  # Fecha y hora actual en formato yyyymmdd_hhmmss
-  TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+    # Obtengo estado HTTP
+    STATUS_CODE=$(curl -LI -o /dev/null -w '%{http_code}\n' -s "$URL")
 
-
- # Registrar en el archivo /var/log/status_url.log
-  echo "$TIMESTAMP - Code:$STATUS_CODE - URL:$URL" |sudo tee -a  "$LOG_FILE"
-
-
-
-#-------------------------#
+    #imprimo la fecha, url y code
+    echo "$TIMESTAMP - URL:$URL - Code:$STATUS_CODE"
+done
 
 IFS=$ANT_IFS
+
